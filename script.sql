b80361236086e5bb77ff884969b1ccaa326c4283
Gustavo <48166275+gustavoIL@users.noreply.github.com>
2019-10-10 08:40:58 -0300

    SQL script do WebSite - FindEats
    Possui quatro tabelas simples:  usuário, receita, ingredientes e a relacionamento entre os dois últimos

---
+++ script.sql
@@ -0,0 +1,29 @@
+create database findEats;
+
+use findEats;
+
+create table usuario(
+	idUsuario int primary key auto_increment,
+    nome varchar(30),
+    sobrenome varchar(30),
+    senha varchar(30),
+    email varchar(30)
+);
+
+create table receita(
+	idReceita int primary key auto_increment,
+    nome varchar(30),
+    modoPreparo varchar(200),
+    porcao int
+);
+
+create table ingredientes(
+	nomeIngr varchar(30) primary key    
+);
+
+create table recipe_ingr(
+	nomeRec varchar(30),
+    nomeIngr varchar(30),
+    foreign key (nomeIngr) references ingredientes(nomeIngr),
+    foreign key (nomeRec) references receita(nome)
+);
