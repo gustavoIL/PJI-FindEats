b3fabc8c84ac4145d1e3e97918a2847a2bd6a7f5
Gustavo <48166275+gustavoIL@users.noreply.github.com>
2019-11-07 09:08:43 -0300

    Script do DB
    Script do estado inicial do banco de dados do projeto, utilizado nas primeiras versões da aplicação

---
+++ script.sql
@@ -0,0 +1,18 @@
+create database findEats;
+
+use findEats;
+
+create table usuario(
+	idUsuario int primary key auto_increment,
+    nome varchar(30),
+    sobrenome varchar(30),
+    senha varchar(30),
+    email varchar(30)
+);
+
+create table receita(
+	idReceita int primary key auto_increment,
+    nome varchar(30),
+    modoPreparo varchar(200),
+    porcao int
+);
